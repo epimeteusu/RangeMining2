53b43ceb76e6a75520b6e1c7e26ef6f99488528b
BuildTools <unconfigured@null.spigotmc.org>
2021-06-16 07:25:50 +0900

    Merge pull request #3 from epimeteusu/aaaa
    Aaaa

--- src/main/java/epi/rangemining/RangeMining.java
+++ src/main/java/epi/rangemining/RangeMining.java
@@ -1,5 +1,6 @@
 package epi.rangemining;
 ///aaaa
+
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -13,11 +14,13 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import java.util.*;
 
 
 public final class RangeMining extends JavaPlugin implements Listener {
-    HashMap<String,Integer> map = new HashMap<>();
+    HashMap<String, Integer> map = new HashMap<>();
+
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -34,10 +37,11 @@ public final class RangeMining extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
         e.setJoinMessage("プレイヤーがログインしました: " + e.getPlayer().getName());
-        map.put(e.getPlayer().getName(),0);
+        map.put(e.getPlayer().getName(), 0);
     }
+
     @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent e){
+    public void onPlayerQuit(PlayerQuitEvent e) {
         e.setQuitMessage("プレイヤーがログインしました: " + e.getPlayer().getName());
     }
 
@@ -55,7 +59,7 @@ public final class RangeMining extends JavaPlugin implements Listener {
                     return Collections.singletonList("small");
                 } else if ("medium".startsWith(args[0])) {
                     return Collections.singletonList("medium");
-                }else if ("large".startsWith(args[0])) {
+                } else if ("large".startsWith(args[0])) {
                     return Collections.singletonList("large");
                 }
             }
@@ -65,33 +69,34 @@ public final class RangeMining extends JavaPlugin implements Listener {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
         // test コマンドの処理
-        if(cmd.getName().equalsIgnoreCase("rm")){
+        if (cmd.getName().equalsIgnoreCase("rm")) {
             sender.sendMessage(sender.getName());
-            if(args[0].equalsIgnoreCase("normal")) {
+            if (args[0].equalsIgnoreCase("normal")) {
                 sender.sendMessage(ChatColor.GREEN + "通常の採掘範囲です");
-                map.put(sender.getName(),0);
+                map.put(sender.getName(), 0);
                 return true;
             }
-            if(args[0].equalsIgnoreCase("small")) {
+            if (args[0].equalsIgnoreCase("small")) {
                 sender.sendMessage(ChatColor.GREEN + "3*3範囲を採掘します");
-                map.put(sender.getName(),1);
+                map.put(sender.getName(), 1);
                 return true;
             }
-            if(args[0].equalsIgnoreCase("medium")) {
+            if (args[0].equalsIgnoreCase("medium")) {
                 sender.sendMessage(ChatColor.GREEN + "5*5範囲を採掘します");
-                map.put(sender.getName(),2);
+                map.put(sender.getName(), 2);
                 return true;
             }
-            if(args[0].equalsIgnoreCase("large")) {
+            if (args[0].equalsIgnoreCase("large")) {
                 sender.sendMessage(ChatColor.GREEN + "7*7範囲を採掘します");
-                map.put(sender.getName(),3);
+                map.put(sender.getName(), 3);
                 return true;
             }
         }
         return false;
     }
+
     @EventHandler
     public void onPlayerJoin(BlockBreakEvent e) {
         Block b = e.getBlock();//破壊ブロックの取得
@@ -101,14 +106,14 @@ public final class RangeMining extends JavaPlugin implements Listener {
             int r = map.get(e.getPlayer().getName());
             Location startLoc = b.getLocation().subtract(r, 0, r);
 
-            for (int x = startLoc.getBlockX(); x < startLoc.getBlockX() + 1+r * 2; x++) {
-                for (int y = startLoc.getBlockY(); y < startLoc.getBlockY() + 1+r * 2; y++) {
-                    for (int z = startLoc.getBlockZ(); z < startLoc.getBlockZ() + 1+r * 2; z++) {
+            for (int x = startLoc.getBlockX(); x < startLoc.getBlockX() + 1 + r * 2; x++) {
+                for (int y = startLoc.getBlockY(); y < startLoc.getBlockY() + 1 + r * 2; y++) {
+                    for (int z = startLoc.getBlockZ(); z < startLoc.getBlockZ() + 1 + r * 2; z++) {
                         Location loc = new Location(startLoc.getWorld(), x, y, z);
                         Block b2 = loc.getBlock();
                         Material brock_type = b2.getType();
-                        if (check_brock(brock_type)){
-                            giveItemList = give_item(giveItemList,e,b2);
+                        if (check_brock(brock_type)) {
+                            giveItemList = give_item(giveItemList, e, b2);
                             //e.getPlayer().getWorld().dropItem(e.getPlayer().getLocation(), giveItemList.get(0));
                             b2.setType(Material.AIR);
                             //b2.breakNaturally();
@@ -116,7 +121,7 @@ public final class RangeMining extends JavaPlugin implements Listener {
                     }
                 }
             }
-            for(int i = 0; i < Objects.requireNonNull(giveItemList).size(); i++){
+            for (int i = 0; i < Objects.requireNonNull(giveItemList).size(); i++) {
                 e.getPlayer().sendMessage(String.valueOf(giveItemList.get(i)));
                 e.getPlayer().getWorld().dropItem(e.getPlayer().getLocation(), giveItemList.get(i));
             }
@@ -126,24 +131,24 @@ public final class RangeMining extends JavaPlugin implements Listener {
 
     public List<ItemStack> give_item(List<ItemStack> giveItemList, BlockBreakEvent e, Block b2) {
         List<ItemStack> new_giveItemList = (List<ItemStack>) b2.getDrops();//新規リストの仮代入
-        if ( giveItemList == null) {
+        if (giveItemList == null) {
             giveItemList = new_giveItemList;
-        }else{
-            for(int i = 0; i < new_giveItemList.size(); i++){//新規リストの要素数ループ
+        } else {
+            for (int i = 0; i < new_giveItemList.size(); i++) {//新規リストの要素数ループ
                 ItemStack get_item = new_giveItemList.get(i);//新入手アイテムの要素を抜き出し
                 boolean contains = false; //重複判定
                 int index = 0; //重複No.
-                for(int f = 0; f < giveItemList.size(); f++){
+                for (int f = 0; f < giveItemList.size(); f++) {
                     ItemStack give_item = giveItemList.get(f);//最終アイテムリストから抜き出し
-                    if(give_item.getType() == get_item.getType()){ //同一アイテム名があった時
+                    if (give_item.getType() == get_item.getType()) { //同一アイテム名があった時
                         contains = true;
                         index = f;
                         break;
                     }
                 }
-                if (contains){
-                    giveItemList.set(index, new ItemStack(get_item.getType(),get_item.getAmount() + giveItemList.get(index).getAmount()));
-                }else{
+                if (contains) {
+                    giveItemList.set(index, new ItemStack(get_item.getType(), get_item.getAmount() + giveItemList.get(index).getAmount()));
+                } else {
                     giveItemList.add(get_item);
                 }
             }
@@ -156,7 +161,7 @@ public final class RangeMining extends JavaPlugin implements Listener {
 
     private boolean check_brock(Material brock_type) {
         boolean check;
-        switch (brock_type){
+        switch (brock_type) {
             case STONE:
             case GRANITE:
             case DIORITE:
